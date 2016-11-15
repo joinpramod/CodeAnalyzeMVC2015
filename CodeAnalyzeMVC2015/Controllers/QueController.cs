16a9dfaa9b26d36c2a551fafb29dc2b4577d3dea
joinpramod <pramodh.suresh@yahoo.com>
2016-11-14 22:45:50 -0500

    update

--- CodeAnalyzeMVC2015/Controllers/QueController.cs
+++ CodeAnalyzeMVC2015/Controllers/QueController.cs
@@ -478,7 +478,7 @@ namespace CodeAnalyzeMVC2015.Controllers
                     strReplyId = dsSolution.Rows[i]["ReplyID"].ToString();
                     //Response no user details
                     string htrResponseNoByDetailsOuterRow = "<tr>";
-                    string htcResponseNoByDetailsOuterCell = "<td style=\"background-color:#F5E8AA; border-radius:10px;\">";
+                    string htcResponseNoByDetailsOuterCell = "<td style=\"background-color:lightgrey; border-radius:10px;\">";
 
                     #region table
                     string htmlTblResponseNoByDetails = "<table style=\"width:100%\">";
