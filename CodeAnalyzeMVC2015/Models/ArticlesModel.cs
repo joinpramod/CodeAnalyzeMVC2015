9343abd4dcf117c1d2ea91c041f0d4c01abb1aa4
joinpramod <pramodh.suresh@yahoo.com>
2016-12-13 14:54:02 -0500

    Update ArticlesModel.cs

--- CodeAnalyzeMVC2015/Models/ArticlesModel.cs
+++ CodeAnalyzeMVC2015/Models/ArticlesModel.cs
@@ -64,6 +64,7 @@ namespace CodeAnalyzeMVC2015.Models
         public string AskedUserDetails { get; set; }
         public string iframeVideoURL { get; set; }
         public string Stars { get; set; }
+         public bool HasDemo { get; set; }
     }
 
     public class QuestionType
