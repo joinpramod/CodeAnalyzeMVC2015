02a44f3123ba5065202fa42f8ea4e347e399abbd
joinpramod <pramodh.suresh@yahoo.com>
2016-12-13 14:45:14 -0500

    Add files via upload

---
+++ CodeAnalyzeMVC2015/Controllers/CodeDemosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class CodeDemosController : Controller
+    {
+        //
+        // GET: /CodeDemos/
+
+        public ActionResult Index(string articleId)
+        {
+            if (string.IsNullOrEmpty(articleId))
+                articleId = "20174";
+            return View(articleId);
+        }
+
+    }
+}
