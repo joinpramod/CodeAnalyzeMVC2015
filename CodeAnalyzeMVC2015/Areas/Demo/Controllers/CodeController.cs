4db5ac5ca7c92a00f44094fbf230003de34898ca
joinpramod <pramodh.suresh@yahoo.com>
2017-01-17 13:04:27 -0500

    Update CodeController.cs

--- CodeAnalyzeMVC2015/Areas/Demo/Controllers/CodeController.cs
+++ CodeAnalyzeMVC2015/Areas/Demo/Controllers/CodeController.cs
@@ -115,4 +115,19 @@ namespace CodeAnalyzeMVC2015.Areas.Demo.Controllers
         #endregion
 
     }
+    
+      #region 20185
+     public class Employee
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        public Employee(string strId, string strName)
+        {
+            ID = strId;
+            Name = strName;
+        }
+    }
+    #endregion
+    
 }
