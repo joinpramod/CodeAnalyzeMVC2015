1a055978add88027ca036d572aa32b0dc3ae2ce9
joinpramod <pramodh.suresh@yahoo.com>
2017-02-14 11:18:28 -0500

    Update Employee.cs

--- CodeAnalyzeMVC2015/Areas/Demo/Models/Employee.cs
+++ CodeAnalyzeMVC2015/Areas/Demo/Models/Employee.cs
@@ -23,4 +23,34 @@ namespace CodeAnalyzeMVC2015.Areas.Demo.Models
         }
     }
     #endregion
+    
+    #region 20192
+    public class MyViewModel
+    {
+        public int? BaseItem { get; set; }
+        public int? ChildItem { get; set; }
+
+        public IEnumerable<SelectListItem> BaseItems
+        {
+            get
+            {
+
+                List<SelectListItem> lstBaseItems = new List<SelectListItem>();
+                lstBaseItems.Add(new SelectListItem
+                {
+                    Value = "Year",
+                    Text = "Year"
+                });
+                lstBaseItems.Add(new SelectListItem
+                {
+                    Value = "Week",
+                    Text = "Week"
+                });
+                return lstBaseItems;
+
+            }
+        }
+    }
+    #endregion
+
 }
